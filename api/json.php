31cd07de3a0b46840006dc9de13eba4f34948c03
Niko Lagman <Lagman.Niko@gmail.com>
2017-10-09 12:34:28 -0400

    Add json handler api

--- api/json.php
+++ api/json.php
@@ -1,8 +1,38 @@
 <?php
-// Retrieves the items.json and returns it as a json array.
-function getJson ($resource) {
+
+/*
+  All input and output of this module, is strictly associative arrays and json.
+  Has no awareness of spec classes.
+*/
+
+// Retrieves a json list and returns it as an associative array if id is not specified.
+// If the id is specified, it will return one document. If it finds nothing, this returns null.
+function getJson ($resource, $id) {
   $json = file_get_contents('./json/' . $resource . '.json');
   $json = json_decode($json, true);
-  return $json;
+
+  if ($id === NULL)
+    return $json;
+  else {
+    foreach ($json as $item) {
+      if ($item['id'] == $id)
+        return $item;
+    }
+    return NULL;
+  }
 }
+
+// Adds an entry to a json source. $data must have toArray() associative
+function saveJson ($resource, $data) {
+  // Retrieve json list. associative.
+  $json = file_get_contents('./json/' . $resource . '.json');
+  $json = json_decode($json, true);
+
+  // Add to list, data must be turned into associative array.
+  array_push($json, $data->toArray());
+
+  // Save associative array as json to json file.
+  file_put_contents('./json/' . $resource . '.json', json_encode($json));
+}
+
 ?>
