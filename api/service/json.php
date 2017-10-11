8821e47e8cc1db15f07853e459ec06ddcba57ba9
Niko Lagman <Lagman.Niko@gmail.com>
2017-10-11 17:06:28 -0400

    Refactor json class for easier api usage

--- api/service/json.php
+++ api/service/json.php
@@ -5,74 +5,115 @@
 */
 
 class Json {
+  /*
+    Retrieves a resource from the json directory.
+
+    parameters
+      $resource - name of the resource [account, item, order]
+    return
+      $array - array list of given resource
+      1 - resource list not available.
+  */
   private static function retriveJson ($resource) {
     $json = file_get_contents('../json/' . $resource . '.json');
+
+    if ($json == false)
+      return 1;
+
     $array = json_decode($json, true);
     return $array;
   }
 
-  // Retrieves a json list and returns it as an associative array if id is not specified.
-  // If the id is specified, it will return one document. If it finds nothing, this returns null.
-  public static function get ($resource, $id) {
-    $array = self::retriveJson($resource);
+  /*
+    Retrieves a resource list or resource object.
+    If given the id of the resource object, it will query for that object.
 
+    parameters
+      $resource - name of the resource [account, item, order]
+      $id - the id of the resource object. this is optional.
+
+    return
+      $array - a resource list is found and is being returned.
+      1 - resource list not found.
+  */
+  public static function get ($resource, $id) {
+    // Retrieve resource.
+    $array = self::retriveJson($resource);
+    // If resource is not availabe.
+    if ($array == 1) return 1;
+
+    // If no id is given, return list.
     if ($id === NULL)
       return $array;
+    // Else if id is given.
     else {
+      // Iterate through list and return it.
       foreach ($array as $item) {
         if ($item['id'] == $id)
           return $item;
       }
-      return NULL;
+      // If not found.
+      return 2;
     }
   }
 
-  // Adds an entry to a json source. $data must have toArray() associative
+  /*
+    Takes an array (associative object) and saves it to a particular resource.
+    This is for new data only.
+
+    parameters
+      $resource - name of the resource [account, item, order]
+      $data - associative array, must have a key for 'id' which must be unique.
+
+    return
+      0 - save was succcessful
+      1 - failed, object with id already exists.
+  */
   public static function save ($resource, $data) {
     $array = self::retriveJson($resource);
 
-    // Check if data already has a record
+    // Iterate through list.
     foreach($array as $i => $doc) {
-      // If it does, do a replacement instead.
-      if ($doc['id'] == $data['id']) {
-        $json[$i] = $data;
-        // Save associative array as json to json file.
-        file_put_contents('../json/' . $resource . '.json', json_encode($json, JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK));
-        return;
-      }
+      // If object with id already exists, return 1 (failed).
+      if ($doc['id'] == $data['id'])
+        return 1;
     }
 
-    // If there is no record, add to list, data must be turned into associative array.
-    // It must also have a uniqueid.
-    $data['id'] = uniqid();
+    // If there is no record, add to list
     array_push($json, $data);
     // Save associative array as json to json file.
     file_put_contents('../json/' . $resource . '.json', json_encode($json, JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK));
     return;
   }
 
-  // Adds an entry to a json source. $data must have toArray() associative
+  /*
+    Takes an array (associative object) and saves it to a particular resource.
+    This is for new data only.
+
+    parameters
+      $resource - name of the resource [account, item, order]
+      $data - associative array, must have a key for 'id' which must be unique.
+
+    return
+      0 - save was succcessful
+      1 - failed, object with id does not exist.
+  */
   public static function update ($resource, $data) {
     $array = self::retriveJson($resource);
 
-    // Check if data already has a record
+    // Iterate through list.
     foreach($json as $i => $doc) {
-      // If it does, do a replacement instead.
+      // If an object with the given id exists, update the doc. (replace it)
       if ($doc['id'] == $data['id']) {
         $json[$i] = $data;
-        // Save associative array as json to json file.
+        // Save back list.
         file_put_contents('../json/' . $resource . '.json', json_encode($json, JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK));
-        return;
+        return 0;
       }
     }
 
-    // If there is no record, add to list, data must be turned into associative array.
-    // It must also have a uniqueid.
-    $data['id'] = uniqid();
-    array_push($json, $data);
-    // Save associative array as json to json file.
-    file_put_contents('../json/' . $resource . '.json', json_encode($json, JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK));
-    return;
+    // If object does not exists, return 1 error.
+    return 1;
   }
 }
 
