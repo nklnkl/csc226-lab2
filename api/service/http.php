13db1e1f28670621a932fa0dc3073e25b5e9c07b
Niko Lagman <Lagman.Niko@gmail.com>
2017-10-11 16:38:08 -0400

    Refactor http module into a static baed class

--- api/service/http.php
+++ api/service/http.php
@@ -1,15 +1,16 @@
-<?php
+<?
 
 /*
-  This module strictly inputs and outputs associative arrays and raw http data.
-  Has no awareness of spec classes.
+  This class strictly deals with HTTP requests.
+  It has no awareness of object classes and uses primitive types and arrays.
 */
 
-// Retrieves the body of a post request and returns it as an associative array.
-function getJsonPost () {
-  $raw = file_get_contents("php://input");
-  $json = json_decode($raw, true);
-  return $json;
+class HTTP {
+  // Retrieves the body of a post request and returns it as an associative array.
+  public static function getJsonPost () {
+    $raw = file_get_contents("php://input");
+    $json = json_decode($raw, true);
+    return $json;
+  }
 }
-
 ?>
