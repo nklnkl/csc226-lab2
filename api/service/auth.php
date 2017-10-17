92c7bcb001c038073cf51916c4a97e8af65dcb83
Niko Lagman <Lagman.Niko@gmail.com>
2017-10-16 20:04:45 -0400

    Add authorize() to Auth class

--- api/service/auth.php
+++ api/service/auth.php
@@ -8,7 +8,7 @@ require_once('../json/account.php');
 class Auth {
 
   /*
-    This will take a user's given email and password to provide a session key.
+    This will take a user's provided email and password to provide a session key.
     If the email and password find a match, a session key is returned.
 
     parameters
@@ -43,7 +43,44 @@ class Auth {
     return $key;
   }
 
-  public static function authorize () {
+  /*
+    This will take a user's provided id and session key to find a match to authorize
+    a session.
+
+    parameters
+      $email *required
+      $password *required
+
+    return
+      0 - session key is valid
+      1 - account list not found, internal server error.
+      2 - account not found or invalid key, client error.
+      3 - given key does not match, client error.
+  */
+  public static function authorize ($id, $key) {
+    $accountData = AccountJson::get($id);
+
+    // If account list was not found.
+    if ($accountData == 1)
+      return 1;
+    // If account was not found.
+    if ($accountData == 2)
+      return 2;
+
+    $account = new Account();
+    $account->createFromArray($accountData);
+
+    // Retrieve session keys from account.
+    $keys = $account->getKeys();
+    // Iterate through keys to find matching session key.
+    foreach ($keys as $k) {
+      // If a match was found.
+      if ($k == $key)
+        return 0;
+    }
+
+    // If a match was never found.
+    return 2;
   }
 }
 ?>
